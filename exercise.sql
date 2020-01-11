ede001016ddb56a9d43f1c0b3d8648afed9c631f
GeorgeWahba <george-wahba@hotmail.com>
2020-01-11 20:24:34 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,6 @@
+use netland;
+SELECT * FROM netland.series;
+SELECT * FROM series WHERE has_won_awards = true;
+SELECT * FROM series WHERE country = "NL";
+SELECT * FROM series WHERE seasons < 5;
+SELECT * FROM series WHERE rating > 2.5;
